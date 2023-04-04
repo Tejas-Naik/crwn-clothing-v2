58f90a3db04503d056dedf3ddd73dcbc265a2155
Tejas-Naik <RNTejas2005@gmail.com>
2023-04-04 10:43:27 -0700

    Setting up our app for stripe

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { store, persistor } from './store/store';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { Elements } from '@stripe/react-stripe-js';
+import { stripePromise } from './utils/stripe/stripe.utils';
+
+import App from './App';
+
+import './index.scss';
+
+const rootElement = document.getElementById('root');
+
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  rootElement
+);
+// serverless functions
+// Stripe
