665db667a7d87cf20023975674530ed93d9079bc
Tejas-Naik <RNTejas2005@gmail.com>
2023-04-05 09:02:48 -0700

    Test

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -5,7 +5,7 @@ import { selectCartTotal } from "../../store/cart/cart.selector";
 import { selectCurrentUser } from "../../store/user/user.selector";
 
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import { FormContainer, PaymentFormContainer, PaymentButton } from "./payment-form.styles";
 
 const PaymentForm = () => {
